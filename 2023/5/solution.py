af84d793c0e9c49f101a0060427239b38e4d8634
Michael Ellsworth <michael.ellsworth7@gmail.com>
2023-12-06 21:14:30 -0700

    day 5 test input

--- 2023/5/solution.py
+++ 2023/5/solution.py
@@ -51,3 +51,130 @@ print(end)
 
 if start <= test_seed <= end:
     print('yes')
+
+print(garden_map['humidity_location'])
+
+garden_map = {
+    'seeds': [79, 14, 55, 13],
+    'seed_soil': {
+        0: {
+            'dest_range_start': 50,
+            'source_range_start': 98,
+            'range_length': 2
+        },
+        1: {
+            'dest_range_start': 52,
+            'source_range_start': 50,
+            'range_length': 48
+        }
+    },
+    'soil_fertilizer': {
+        0: {
+            'dest_range_start': 0,
+            'source_range_start': 15,
+            'range_length': 37
+        },
+        1: {
+            'dest_range_start': 37,
+            'source_range_start': 52,
+            'range_length': 2
+        },
+        2: {
+            'dest_range_start': 39,
+            'source_range_start': 0,
+            'range_length': 15
+        }  
+    },
+    'fertilizer_water': {
+        0: {
+            'dest_range_start': 49,
+            'source_range_start': 53,
+            'range_length': 8
+        },
+        1: {
+            'dest_range_start': 0,
+            'source_range_start': 11,
+            'range_length': 42
+        },
+        2: {
+            'dest_range_start': 42,
+            'source_range_start': 0,
+            'range_length': 7
+        },
+        3: {
+            'dest_range_start': 57,
+            'source_range_start': 7,
+            'range_length': 4
+        },
+    },
+    'water_light': {
+        0: {
+            'dest_range_start': 88,
+            'source_range_start': 18,
+            'range_length': 7
+        },
+        1: {
+            'dest_range_start': 18,
+            'source_range_start': 25,
+            'range_length': 70
+        },
+    },
+    'light_temp': {
+        0: {
+            'dest_range_start': 45,
+            'source_range_start': 77,
+            'range_length': 23
+        },
+        1: {
+            'dest_range_start': 81,
+            'source_range_start': 45,
+            'range_length': 19
+        },
+        2: {
+            'dest_range_start': 68,
+            'source_range_start': 64,
+            'range_length': 13
+        },
+    },
+    'temp_humidity': {
+        0: {
+            'dest_range_start': 0,
+            'source_range_start': 69,
+            'range_length': 1
+        },
+        1: {
+            'dest_range_start': 1,
+            'source_range_start': 0,
+            'range_length': 69
+        },
+    },
+    'humidity_location': {
+        0: {
+            'dest_range_start': 60,
+            'source_range_start': 56,
+            'range_length': 37
+        },
+        1: {
+            'dest_range_start': 56,
+            'source_range_start': 93,
+            'range_length': 4
+        },
+    },
+}
+print(garden_map)
+
+test_seed = garden_map['seeds'][0]
+print(test_seed)
+
+index_map = 24
+test_seed_soil_map = garden_map['seed_soil'][index_map]
+print(test_seed_soil_map)
+
+start = test_seed_soil_map['source_range_start']
+end = test_seed_soil_map['source_range_start'] + test_seed_soil_map['range_length']
+print(start)
+print(end)
+
+
+if start <= test_seed <= end:
+    print('yes')
