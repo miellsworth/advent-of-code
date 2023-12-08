ec9285e4614341339a1f939bbc97ac449bcb188b
Michael Ellsworth <michael.ellsworth7@gmail.com>
2023-12-08 08:47:29 -0700

    minor change to day 7 input

--- 2023/7/solution.py
+++ 2023/7/solution.py
@@ -1,9 +1,13 @@
 # Read input file as list of strings, remove "\n"
-content = []
+input = []
 with open('input.txt', 'r') as f:
     for line in f:
         if not line.isspace():
-            content.append(line.rstrip("\n").split())
+            line = line.rstrip("\n")
+            line = line.split()
+            input.append(line)
+
+print(input)
 
 # Solution - part 1
 input = [
